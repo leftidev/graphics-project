ae4d1b90bc2ec9ded3a4c95a155df3f5917466a6
Jesper Bergström <jes.bergstrom@gmail.com>
2022-01-25 19:49:04 +0200

    Made template folder and CMakeLists for unit tests

---
+++ UnitTests/WindowTest.cpp
@@ -0,0 +1,15 @@
+// TODO: Include your class to test here.
+#define BOOST_TEST_MODULE WindowTest
+#include <boost/test/included/unit_test.hpp>
+#include "Window.h"
+
+BOOST_AUTO_TEST_CASE(WindowTest)
+{
+    // To simplify this example test, let's suppose we'll test 'float'.
+    // Some test are stupid, but all should pass.
+    float x = 9.5f;
+
+    BOOST_CHECK(x != 0.0f);
+    BOOST_CHECK_EQUAL((int)x, 9);
+    BOOST_CHECK_CLOSE(x, 9.5f, 0.0001f); // Checks differ no more then 0.0001%
+}
