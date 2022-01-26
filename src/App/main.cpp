9b628e382698e290ac5375c0b3edcfd25d297037
Jesper Bergström <jes.bergstrom@gmail.com>
2022-01-26 19:22:27 +0200

    Created process input function

--- src/App/main.cpp
+++ src/App/main.cpp
@@ -6,27 +6,31 @@ void errorCallback(int error, const char* description) {
    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
 }
 
-void colorFlicker(float time) {
-   glClearColor(time, time / 10.0f, time / 50.0f, 1.0f);
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
 
 int main(int argc, char** argv) {
    (void)argc;
    (void)argv;
-
-   
    
    glfwSetErrorCallback(errorCallback);
 
    Window window;
    window.init(800, 600, "Hello");
 
+   // render loop
    while (!glfwWindowShouldClose(window.getHandle())) {
-      float time = (float)glfwGetTime() * 0.1f;
-      colorFlicker(time);
+      // input
+      processInput(window.getHandle());
 
+      // rendering commands
+      glClearColor(0.5f, 0.5f, 0.5f, 0.5f);
       glClear(GL_COLOR_BUFFER_BIT);
    
+      // check and call events and swap the buffers
       glfwSwapBuffers(window.getHandle());
       glfwPollEvents();
    }
