dd6761e397748305c4127ccabf505d6526b82d57
Jesper Bergström <jes.bergstrom@gmail.com>
2022-01-27 19:07:29 +0200

    Some vertex struct testing

--- src/App/main.cpp
+++ src/App/main.cpp
@@ -15,6 +15,8 @@ void processInput(GLFWwindow *window)
         glfwSetWindowShouldClose(window, true);
 }
 
+
+
 int main(int argc, char** argv) {
    (void)argc;
    (void)argv;
@@ -24,6 +26,7 @@ int main(int argc, char** argv) {
    Window window;
    window.init(800, 600, "Hello");
 
+
    // Specify 3 vertices with each vertex having a 3D position (x, y, z)
    // To render a 2D triangle, each vertex has z coord of 0.0
    float vertices[] = {
@@ -31,9 +34,46 @@ int main(int argc, char** argv) {
      0.5f, -0.5f, 0.0f,
      0.0f,  0.5f, 0.0f
    };  
+
+   (void)vertices;
+
+   // You want to go from NDC (nx, ny, nz) to view space (vx, vy, vz)
+   // Fortunately this is pretty simple...
+   //vz = -tz / (nz - sz)  <-- Evaluate this first!
+   //vx = (-vz * nx) / sx
+   //vy = (-vz * ny) / sy
    
-   Shader shader;
-   shader.init(vertices);
+   //Shader shader;
+   //shader.init(vertices);
+
+   Vertex vertex1;
+   vertex1.z = 0.0f;
+   vertex1.x = -0.5f;
+   vertex1.y = -0.5f;
+
+   Vertex vertex2;
+   vertex2.z = 0.0f;
+   vertex2.x = 0.5f;
+   vertex2.y = -0.5f;
+
+   Vertex vertex3;
+   vertex3.z = 0.0f;
+   vertex3.x = 0.0f;
+   vertex3.y = 0.5f;
+
+   Triangle triangle;
+   triangle.vertices[0] = vertex1;
+   triangle.vertices[1] = vertex2;
+   triangle.vertices[2] = vertex3;
+
+   Vertex vertexData[3];
+   vertexData[0] = {-0.5f, -0.5f, 0.0f};
+   vertexData[1] = {0.5f, -0.5f, 0.0f};
+   vertexData[2] = {0.0f, 0.5f, 0.0f};
+
+   Shader shader2;
+   shader2.init2(vertexData);
+
    // TODO: Do we need to specify glViewPort?
 
    // render loop
@@ -45,8 +85,8 @@ int main(int argc, char** argv) {
       glClearColor(0.5f, 0.5f, 0.5f, 0.5f);
       glClear(GL_COLOR_BUFFER_BIT);
    
-      glUseProgram(shader.shaderProgram);
-      glBindVertexArray(shader.VAO);
+      glUseProgram(shader2.shaderProgram);
+      glBindVertexArray(shader2.VAO);
       glDrawArrays(GL_TRIANGLES, 0, 3);
 
       // check and call events and swap the buffers
--- src/Renderer/Shader.h
+++ src/Renderer/Shader.h
@@ -7,11 +7,22 @@
 void checkShaderCompileErrors(unsigned int shader);
 void checkShaderLinkErrors(unsigned int shaderProgram);
 
+struct Vertex {
+   float z;
+   float x;
+   float y;
+};
+
+struct Triangle {
+   Vertex vertices[3];
+};
+
 class Shader {
 public:
     Shader() {};
     ~Shader() {};
     void init(float (&vertices)[9]);
+    void init2(Vertex (&vertexData)[3]);
 
     unsigned int VAO;
     unsigned int shaderProgram;
